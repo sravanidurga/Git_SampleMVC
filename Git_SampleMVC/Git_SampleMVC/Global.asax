f61397d38ca642668d0cf6ff391e5af3f6ad0e4a
sravanidurga <sravanidurga1504@gmail.com>
2019-12-30 12:13:49 +0530

    Git Sample MVC

---
+++ Git_SampleMVC/Git_SampleMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Git_SampleMVC.MvcApplication" Language="C#" %>
---
+++ Git_SampleMVC/Git_SampleMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Git_SampleMVC.Startup))]
+namespace Git_SampleMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
